c828c174f20a20324203912180229382cef5d99a
kennedy <kennedy.adams@uoit.net>
2020-10-06 16:12:53 -0400

    Unity and Start Scene done

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void play()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void end()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+
+    public void start()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
